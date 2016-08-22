8e5a7f291fb756fb98229e01ea42693f649a87ba
James Williams <jwilliams@pivotal.io>
2016-08-22 12:10:35 -0400

    bbb

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-        System.out.println("zzz");
+        System.out.println("aaa");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
